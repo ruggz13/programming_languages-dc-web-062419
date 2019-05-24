8671bbc5361ae4a7e00a4858a60a9df0f7e8d9b8
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-24 17:32:31 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |type, language_hash|
     language_hash.each do |language, attributes|
-      new_hash[name] ||= attributes
+      new_hash[language] ||= attributes
       binding.pry
 
     end
